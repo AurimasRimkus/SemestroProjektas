ed8c7292c6182c2f18aee0557581b4603b5327fd
AurimasRimkus <me@aurimasrimkus.eu>
2018-03-06 13:48:59 +0200

    Pradinis symfony projektas

---
+++ demoProj/src/Controller/DefaultController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+
+class DefaultController extends AbstractController
+{
+    public function index()
+    {
+        return new Response(
+            'Pagrindinis puslapis'
+        );
+    }
+
+    public function show()
+    {
+        return $this->render('index.html.twig', [
+
+        ]);
+    }
+}
---
+++ demoProj/src/Controller/LuckyController.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class LuckyController
+{
+    public function number()
+    {
+        $number = mt_rand(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+
+}
+
