318e1bb7523c3bfa058c9cfa04c3950ac3bbe074
mantinjio <putriusmantas1997@gmail.com>
2018-03-12 19:59:05 +0200

    Back-end logic for login

---
+++ namuDarbai/Models/Student.php
@@ -0,0 +1,17 @@
+<?php
+namespace Models;
+
+class Student
+{
+    /**
+     * @var string
+     */
+    public $username;
+
+    /**
+     * @var string
+     */
+    public $password;
+
+
+}
---
+++ namuDarbai/login.php
@@ -0,0 +1,22 @@
+<?php
+include "Models/Student.php";
+
+use Models\Student;
+
+
+$password = $_POST['pass'];
+
+$newStudent = new Student();
+$newStudent->username = $_POST['login'];
+$newStudent->password = $password;
+
+$existingStudent = new Student();
+$existingStudent->username = "Dievas";
+$existingStudent->password = "Aurimas";
+
+if($newStudent->username === $existingStudent->username &&
+    $newStudent->password === $existingStudent->password){
+    echo "Sveikas prisijunges, " . $newStudent->username;
+}else{
+    echo "Blogi prisijungimo duomenys.";
+}
