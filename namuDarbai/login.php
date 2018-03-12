932dc0d9d4c678ba62743165fe302ca98eef4095
mantinjio <putriusmantas1997@gmail.com>
2018-03-12 23:28:32 +0200

    Fixing session after logging in.

--- namuDarbai/login.php
+++ namuDarbai/login.php
@@ -4,6 +4,11 @@ include "Models/Student.php";
 use Models\Student;
 session_start();
 
+if(isset($_SESSION['username'])){
+    header("Location: index.php");
+    die();
+}
+
 //Instead of using a database, we're creating a demo account.
 //In a real-life application a database would be used which would select
 //user from database with username that was inputted, and get password hash of that user.
