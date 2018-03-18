4e75d9731cf244d4eab040ec85637790a7f6a658
mantinjio <putriusmantas1997@gmail.com>
2018-03-18 05:13:36 +0200

    System for registration

--- demoProj/src/Controller/RegistrationController.php
+++ demoProj/src/Controller/RegistrationController.php
@@ -2,27 +2,54 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
-
+/**
+ * Class RegistrationController
+ * @package App\Controller
+ */
 class RegistrationController extends AbstractController
 {
-    public function index()
-    {
-        return new Response(
-            'Registration page'
-        );
-    }
-
     /**
      * @Route("/register", name="register")
+     * @param Request $request
+     * @param UserPasswordEncoderInterface $encoder
+     * @return Response
      */
-    public function show()
+    public function register(Request $request, UserPasswordEncoderInterface $encoder)
     {
-        return $this->render('register.html.twig', [
+        $user = new User();
 
-        ]);
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $em = $this->getDoctrine()->getManager();
+
+            $password = $encoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($password);
+
+            $time = new \DateTime();
+            $user->setRegistrationDate($time);
+            $user->setLastLoginTime($time);
+
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('login');
+        }
+
+        return $this->render('register.html.twig', array(
+           'form'=>$form->createView()
+        ));
     }
 }
