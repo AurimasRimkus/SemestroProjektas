d677e8828834900406254649c1401d9f1ace7c35
Aurimas Rimkus <me@aurimasrimkus.eu>
2019-12-02 01:49:07 +0200

    order test remake

--- demoProj/tests/Entity/OrderTest.php
+++ demoProj/tests/Entity/OrderTest.php
@@ -15,42 +15,75 @@ class OrderTest extends TestCase
 //    private $em;
 //    private $doctrine;
 
-    public function testOrderEntity(){
-        $user = new User();
-        $car = new Car();
-        $profile = new Profile();
-        $repair = new Repair();
-
-        $order = new Order();
-
-        $order->addRepairs($repair);
-        $order->setProfile($profile);
-        $order->setDuration(10);
-        $order->setCar($car);
-        $order->setComment('comment');
-        $order->setFinishDate(new \DateTime('2011-10-10'));
-        $order->setIsDone(true);
-        $order->setStartDate(new \DateTime('2000-10-10'));
-
-        $this->assertContains($repair , $order->getRepairs());
-        $this->assertEquals($profile, $order->getProfile());
-        $this->assertEquals(10, $order->getDuration());
-        $this->assertEquals($car, $order->getCar());
-        $this->assertEquals('comment', $order->getComment());
-        $this->assertEquals(new \DateTime('2011-10-10'), $order->getFinishDate());
-        $this->assertEquals(true, $order->getIsDone());
-        $this->assertEquals(new \DateTime('2000-10-10'), $order->getStartDate());
-
-        $order->setUser($user);
-        $this->assertEquals($user, $order->getUser());
-    }
-
     public function testId(){
         $order = new Order();
         $order->setId("1");
         $this->assertEquals("1", $order->getId());
     }
-
-
-
+	
+	public function testRepairs() {
+		$order = new Order();
+		$repair = new Repair();
+		$order->addRepairs($repair);
+		$this->assertContains($repair , $order->getRepairs());
+	}
+	
+	public function testRepairs() {
+		$order = new Order();
+		$profile = new Profile();
+		$order->setProfile($profile);
+		$this->assertEquals($profile, $order->getProfile());
+	}
+	
+	public function testOrderRepairs() {
+		$order = new Order();
+		$order->setDuration(10);
+		$this->assertEquals(10, $order->getDuration());
+	}
+	
+	public function testOrderDuration() {
+		$order = new Order();
+		$order->setDuration(10);
+		$this->assertEquals(10, $order->getDuration());
+	}
+	
+	public function testOrderCar() {
+		$order = new Order();
+		$car = new Car();
+		$order->setCar($car);
+		$this->assertEquals($car, $order->getCar());
+	}
+	
+	public function testOrderComment() {
+		$order = new Order();
+		$order->setComment('comment');
+		$this->assertEquals('comment', $order->getComment());
+	}
+	
+	public function testOrderFinishDate() {
+		$order = new Order();
+		$order->setFinishDate(new \DateTime('2011-10-10'));
+		$this->assertEquals(new \DateTime('2011-10-10'), $order->getFinishDate());
+	}
+	
+	public function testOrderDoneStatus() {
+		$order = new Order();
+		$order->setIsDone(true);
+		$this->assertEquals(true, $order->getIsDone());
+		$order->setIsDone(false);
+		$this->assertEquals(false, $order->getIsDone());
+	}
+	
+	public function testOrderStartDate() {
+		$order = new Order();
+		$order->setStartDate(new \DateTime('2000-10-10'));
+		$this->assertEquals(new \DateTime('2000-10-10'), $order->getStartDate());
+	}
+	
+	public function testOrderUser() {
+		$order = new Order();
+		$user = new User();
+		$order->setUser($user);
+        $this->assertEquals($user, $order->getUser());
+	}
 }
