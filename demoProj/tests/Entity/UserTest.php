2e694291914674d46c0f56717e81f5366ab6df82
Aurimas Rimkus <me@aurimasrimkus.eu>
2019-12-02 01:37:11 +0200

    remade user test

--- demoProj/tests/Entity/UserTest.php
+++ demoProj/tests/Entity/UserTest.php
@@ -9,42 +9,104 @@ use Symfony\Bundle\FrameworkBundle\Tests\TestCase;
 
 class UserTest extends TestCase
 {
-    public function testUserEntity(){
-        $profile = new Profile();
-        $order = new Order();
-
-        $user = new User();
-        $user->setId("99");
-        $user->setUsername("Name");
-        $user->setEmail("mail@mail.com");
-        $user->setPassword("pass");
-        $user->setNewPassword("pass2");
-        $user->setPasswordResetToken("token");
-        $user->setRole(3);
-        $user->setRegistrationDate(new \DateTime('2000-10-10'));
-        $user->setLastLoginTime(new \DateTime('2011-10-10'));
-        $user->setIsActive(true);
-        $user->setIsDeleted(true);
-        $user->setRegistrationToken("tokenreg");
-        $user->setProfile($profile);
-        $user->setOrders($order);
-
-        $this->assertEquals("99", $user->getId());
-        $this->assertEquals("Name", $user->getUsername());
-        $this->assertEquals("mail@mail.com", $user->getEmail());
-        $this->assertEquals("pass", $user->getPassword());
-        $this->assertEquals("pass2", $user->getNewPassword());
-        $this->assertEquals("token", $user->getPasswordResetToken());
-        $this->assertEquals(3, $user->getRole());
-        $this->assertEquals(new \DateTime('2000-10-10'), $user->getRegistrationDate());
-        $this->assertEquals(new \DateTime('2011-10-10'), $user->getLastLoginTime());
-        $this->assertEquals(true, $user->getIsActive());
-        $this->assertEquals(true, $user->getIsDeleted());
-        $this->assertEquals("tokenreg", $user->getRegistrationToken());
-        $this->assertEquals($profile, $user->getProfile());
-        $this->assertEquals($order, $user->getOrders());
-        $this->assertEquals(null, $user->getSalt());
-        $this->assertEquals(array ('ROLE_USER'), $user->getRoles());
-        $this->assertEquals(null,  $user->eraseCredentials());
-    }
+	public function testId () {
+		$user = new User();
+		$user->setId("99");
+		$this->assertEquals("99", $user->getId());
+	}
+	
+	public function testUsername () {
+		$user = new User();
+		$user->setUsername("Name");
+		$this->assertEquals("Name", $user->getUsername());
+	}
+	
+	public function testEmail () {
+		$user = new User();
+		$user->setEmail("mail@mail.com");
+		$this->assertEquals("mail@mail.com", $user->getEmail());
+	}
+	
+	public function testPassword () {
+		$user = new User();
+		$user->setPassword("pass");
+		$this->assertEquals("pass", $user->getPassword());
+	}
+	
+	public function testNewPassword () {
+		$user = new User();
+		$user->setNewPassword("pass2");
+		$this->assertEquals("pass2", $user->getNewPassword());
+	}
+	
+	public function testPasswordResetToken () {
+		$user = new User();
+		$user->setPasswordResetToken("token");
+		$this->assertEquals("token", $user->getPasswordResetToken());
+	}
+	
+	public function testRole () {
+		$user = new User();
+		$user->setRole(3);
+		$this->assertEquals(3, $user->getRole());
+	}
+	
+	public function testRegistrationDate () {
+		$user = new User();
+		$user->setRegistrationDate(new \DateTime('2000-10-10'));
+		$this->assertEquals(new \DateTime('2000-10-10'), $user->getRegistrationDate());
+	}
+	
+	public function testLastLoginTime () {
+		$user = new User();
+		$user->setLastLoginTime(new \DateTime('2011-10-10'));
+		$this->assertEquals(new \DateTime('2011-10-10'), $user->getLastLoginTime());
+	}
+	
+	public function testIsActive () {
+		$user = new User();
+		$user->setIsActive(true);
+		$this->assertEquals(true, $user->getIsActive());
+	}
+	
+	public function testIsDeleted () {
+		$user = new User();
+		$user->setIsDeleted(true);
+		$this->assertEquals(true, $user->getIsDeleted());
+	}
+	
+	public function testRegistrationToken () {
+		$user = new User();
+		$user->setRegistrationToken("tokenreg");
+		$this->assertEquals("tokenreg", $user->getRegistrationToken());
+	}
+	
+	public function testProfile() {
+		$user = new User();
+		$profile = new Profile();
+		$user->setProfile($profile);
+		$this->assertEquals($profile, $user->getProfile());
+	}
+	
+	public function testOrder() {
+		$user = new User();
+		$order = new Order();
+		$user->setOrders($order);
+		$this->assertEquals($order, $user->getOrders());
+	}
+	
+	public function testSalt() {
+		$user = new User();
+		$this->assertEquals(null, $user->getSalt());
+	}
+	
+	public function testRoles() {
+		$user = new User();
+		$this->assertEquals(array ('ROLE_USER'), $user->getRoles());
+	}
+	
+	public function testCredentials() {
+		$user = new User();
+		$this->assertEquals(null,  $user->eraseCredentials());
+	}
 }
